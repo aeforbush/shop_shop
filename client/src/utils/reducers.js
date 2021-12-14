0e38b8ccda267e16bd287f847fa5ef78fc11b9af
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-14 10:22:08 -0700

    added shopping cart test and reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -7,10 +7,10 @@ import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   CLEAR_CART,
-  TOGGLE_CART
+  TOGGLE_CART,
 } from "./actions";
 
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
 export const reducer = (state, action) => {
   switch (action.type) {
@@ -35,12 +35,50 @@ export const reducer = (state, action) => {
         currentCategory: action.currentCategory,
       };
     case ADD_TO_CART:
-    return {
-      // include ...state to perserve everything else on state
-      ...state,
-      cartOpen: true,
-      cart: [...state.cart, action.product]
-    };
+      return {
+        // include ...state to perserve everything else on state
+        ...state,
+        cartOpen: true,
+        // add action.property at the end of the array
+        cart: [...state.cart, action.product],
+      };
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
 
     // if it's none of these actions, don't update state at all
     default:
@@ -51,5 +89,5 @@ export const reducer = (state, action) => {
 // this func helps initialize our global state object and automatically runs it through the custom reducer
 // useReducer Hook is meant specifically for managing a greater level of state
 export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
+  return useReducer(reducer, initialState);
 }
