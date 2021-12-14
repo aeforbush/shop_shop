b9a81c090f97b67eb8cb10abe265e63cdfba6cfa
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-14 08:48:52 -0700

    added cart actions

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -6,3 +6,9 @@ export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
 // kind of like a connecting piece of data for the previous two actions... selecting from state created by Update_Cat and display for that category from the list created by Update_Products
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
 
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
