e43aa449267c015c8cb3557f49ac6f6a1fa63ff7
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-16 16:39:43 -0700

    Stripe functionality up and running

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,16 +1,24 @@
+import { useLazyQuery } from '@apollo/client';
 import React, { useEffect } from "react";
 import CartItem from "../CartItem";
-
-import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-import { idbPromise } from "../../utils/helpers";
 
 import Auth from "../../utils/auth";
 import "./style.css";
 
+import { useStoreContext } from "../../utils/GlobalState";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+
+
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
   // console.log(state);
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
     async function getCart() {
@@ -23,6 +31,14 @@ const Cart = () => {
     }
     // useEffect's dependency array - executes when value has changed in the array
   }, [state.cart.length, dispatch]);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -46,6 +62,20 @@ const Cart = () => {
     );
   }
 
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+
   return (
 <div className="cart">
   <div className="close" onClick={toggleCart}>[close]</div>
@@ -59,7 +89,7 @@ const Cart = () => {
         <strong>Total: {calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
+            <button onClick={submitCheckout}>
               Checkout
             </button>
             :
