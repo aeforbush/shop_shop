d4f4c85a0b40987d5dbd57ddf404bb21a92cf9ac
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-12-16 14:35:27 -0700

    products cached for offline reference

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,6 +14,7 @@ import {
 } from "../utils/actions";
 
 import Cart from "../components/Cart";
+import { idbPromise } from "../utils/helpers";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -53,13 +54,29 @@ function Detail() {
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find((product) => product._id === id));
-    } else if (data) {
+    }
+    // retrieved from server 
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        })
+      })
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
